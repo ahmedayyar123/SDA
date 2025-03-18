39ed18776baf03dfe29e36d20dcb47dfcb4a97d2
ahmedayyar123 <okhoori3399@gmail.com>
2025-03-18 14:56:11 +0500

    SDA _ Creator Principle

---
+++ creator_without_priciple.java
@@ -0,0 +1,8 @@
+public class creator_without_priciple {
+    public static void main(String[] args) {
+        String title = "The Art of Thinking"; // No structured creation
+        String author = "John Doe";
+
+        System.out.println("Book: " + title + " by " + author);
+    }
+}
